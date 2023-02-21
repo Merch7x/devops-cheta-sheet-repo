7f221e1161da00b1fdefb584a480bc8e8fee7fbe
Tim <aquamaingi@gmail.com>
2023-02-21 20:45:55 +0300

    namespace setting

--- Kubernetes/rolling-updates.sh
+++ Kubernetes/rolling-updates.sh
@@ -1,3 +1,6 @@
 #To perform app updates change the image of the current deployment
 
 kubectl set image deployments/my-deployment my-container=nginx:latest
+
+# To roll back an update
+kubectl rollout undo deployments/my-deployment
