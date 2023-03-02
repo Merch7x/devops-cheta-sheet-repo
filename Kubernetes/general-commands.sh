c52cf1a0fba58e8c2357a93e951538564d979bd1
Merch7x <tim@DESKTOP-9LIPQLA>
2023-03-02 20:51:54 +0300

    added cp command

--- Kubernetes/general-commands.sh
+++ Kubernetes/general-commands.sh
@@ -9,3 +9,6 @@ kubectl edit resource/<resource-name>
 
 #To change namespace
 kubectl config set-context --current --namespace=<namespace>
+
+#To copy files from local to prod
+kubectl cp /tmp/index.php nginx-phpfpm-dp-5cccd45499-lghgl:/var/www/html/
