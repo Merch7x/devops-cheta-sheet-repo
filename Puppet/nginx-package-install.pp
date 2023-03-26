0a762ea0b5792dfefbea30e2fab433b1b9f2e64e
Merch7x <tim@DESKTOP-9LIPQLA>
2023-03-26 19:45:12 +0300

    add run instructions

--- Puppet/nginx-package-install.pp
+++ Puppet/nginx-package-install.pp
@@ -11,3 +11,9 @@ class nginx_installer {
 node 'stapp01.stratos.xfusioncorp.com', 'stapp02.stratos.xfusioncorp.com', 'stapp03.stratos.xfusioncorp.com' {
   include nginx_installer
 }
+
+
+# to validate puppet file
+puppet parser validate <filename>
+#to run on agent
+puppet agent -tv
